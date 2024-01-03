0a2e220fa8d492ca86556741e17a92da0c7a56a3
Oleg Ten <tennet0505@gmail.com>
2024-01-03 12:20:33 +0100

    bump ver 0.0.91

--- run_rails_loki_exporter_dev.sh
+++ run_rails_loki_exporter_dev.sh
@@ -6,5 +6,5 @@ echo " "
 echo "---- running command: build ruby_for_grafana_loki.gemspec ----"
 gem build ruby_for_grafana_loki.gemspec
 echo " "
-echo "---- running command: install ruby_for_grafana_loki-0.0.90.gem ----"
-gem install ruby_for_grafana_loki-0.0.90.gem
+echo "---- running command: install ruby_for_grafana_loki-0.0.91.gem ----"
+gem install ruby_for_grafana_loki-0.0.91.gem
